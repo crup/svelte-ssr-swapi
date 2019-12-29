f92f6c90cb53cf246ae75304f3a81e266b28e78e
Rajender Joshi <i@sonu.tv>
2019-12-30 02:06:44 +0530

    setting up is loading

--- src/preloaders/common-pages.js
+++ src/preloaders/common-pages.js
@@ -1,9 +1,12 @@
 export default function ({ path }) {
-    let isLoading = false;
+    let isLoading = true;
+    console.log(isLoading);
     const baseUrl = "https://swapi.co/api";
     return this.fetch(`${baseUrl}${path}`)
         .then(r => r.json())
         .then(cards => {
+            isLoading = false;
+            console.log(isLoading);
             return { cards: cards.results, isLoading };
         }
     );
